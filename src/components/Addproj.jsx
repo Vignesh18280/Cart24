3485f206ca184eb3b578f70f541216619d2d8260
KANABOINA VIGNESH <123869203+Vignesh18280@users.noreply.github.com>
2023-10-13 14:50:44 +0000

    changes and changes

--- src/components/Addproj.jsx
+++ src/components/Addproj.jsx
@@ -1,40 +1,51 @@
 import React from 'react'
 import './Addproj.css'
+import { useState } from 'react';
 
 function Addproj() {
+    const [title,settitle] = useState('');
+    const [description,setdescription] = useState('');
+    const [price,setprice] = useState('');
+    const [rating,setrating] = useState('');
+    const [thumbnail,setthumbnail] = useState('');
+
+    let handlesubmit = async(e) =>{
+        e.preventDefault();
+        console.log(title);
+    }
     return(
         <div>
             <div class="login-box">
             <h2>Add project</h2>
             <form>
                 <div class="user-box">
-                <input type="text" name="" required=""/>
+                <input type="text" name="title" value={title} onChange={(e)=>{settitle(e.target.value)}} required=""/>
                 <label>title</label>
                 </div>
                 <div class="user-box">
-                <input type="text" name="" required=""/>
+                <input type="text" name="" value={description} onChange={(e)=>{setdescription(e.target.value)}} required=""/>
                 <label>description</label>
                 </div>
                 <div class="user-box">
-                <input type="text" name="" required=""/>
+                <input type="text" name="" value={price} onChange={(e)=>{setprice(e.target.value)}} required=""/>
                 <label>price</label>
                 </div>
                 <div class="user-box">
-                <input type="text" name="" required=""/>
+                <input type="text" name="" value={rating} onChange={(e)=>{setrating(e.target.value)}} required=""/>
                 <label>rating</label>
                 </div>
                 <div class="user-box">
-                <input type="text" name="" required=""/>
+                <input type="text" name="" value={thumbnail} onChange={(e)=>{setthumbnail(e.target.value)}} required=""/>
                 <label>thumbnail</label>
                 </div>
 
-                <a href="#">
+                <button onSubmit={handlesubmit} className='a'>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
                 ADD
-                </a>
+                </button>
             </form>
             </div>
         </div>
